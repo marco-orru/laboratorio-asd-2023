60ae859ebc8615916377d74221197e4d52ca3ae6
Marco Orrù <marco.orru@edu.unito.it>
2024-01-27 17:31:50 +0100

    AbstractQueue.top method

--- ex3-4/ASD/src/org/unito/asd/AbstractQueue.java
+++ ex3-4/ASD/src/org/unito/asd/AbstractQueue.java
@@ -6,49 +6,56 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * Defines an interface representing a queue with generic elements.
+ *
  * @param <E> The type of elements in the queue.
  */
 public interface AbstractQueue<E> {
-    /**
-     * Checks whether the queue is empty or not.
-     * @return {@code true} if the queue is empty, {@code false} otherwise.
-     * @implSpec This operation shall have constant time complexity O(1).
-     */
-    @Contract(pure = true)
-    boolean empty();
+  /**
+   * Checks whether the queue is empty or not.
+   *
+   * @return {@code true} if the queue is empty, {@code false} otherwise.
+   * @implSpec This operation shall have constant time complexity O(1).
+   */
+  @Contract(pure = true)
+  boolean empty();
 
-    /**
-     * Adds an element to the queue.
-     * @param element The element to be added.
-     * @return {@code true} if the element is successfully added, {@code false} otherwise.
-     */
-    boolean push(@NotNull E element);
+  /**
+   * Adds an element to the queue.
+   *
+   * @param element The element to be added.
+   * @return {@code true} if the element is successfully added, {@code false} otherwise.
+   */
+  boolean push(E element);
 
-    /**
-     * Checks whether a specific element is present in the queue or not.
-     * @param element The element to check for existence.
-     * @return {@code true} if the element is present, {@code false} otherwise.
-     * @implSpec This operation shall have constant time complexity O(1).
-     */
-    @Contract(pure = true)
-    boolean contains(@NotNull E element);
+  /**
+   * Checks whether a specific element is present in the queue or not.
+   *
+   * @param element The element to check for existence.
+   * @return {@code true} if the element is present, {@code false} otherwise.
+   * @implSpec This operation shall have constant time complexity O(1).
+   */
+  @Contract(pure = true)
+  boolean contains(E element);
 
-    /**
-     * Accesses the element at the top of the queue without removing it.
-     * @return The element at the top of the queue.
-     */
-    @Contract(pure = true)
-    @Nullable E top();
+  /**
+   * Accesses the element at the top of the queue without removing it.
+   *
+   * @return The element at the top of the queue.
+   * @implSpec This operation shall have constant time complexity O(1).
+   */
+  @Contract(pure = true)
+  E top();
 
-    /**
-     * Removes the element at the top of the queue.
-     */
-    void pop();
+  /**
+   * Removes the element at the top of the queue.
+   */
+  void pop();
 
-    /**
-     * Removes a specific element from the queue.
-     * @param element The element to be removed.
-     * @return {@code true} if the element is successfully removed, {@code false} otherwise.
-     */
-    boolean remove(@NotNull E element);
+  /**
+   * Removes a specific element from the queue.
+   *
+   * @param element The element to be removed.
+   * @return {@code true} if the element is successfully removed, {@code false} otherwise.
+   */
+  boolean remove(E element);
 }
--- ex3-4/ASD/src/org/unito/asd/PriorityQueue.java
+++ ex3-4/ASD/src/org/unito/asd/PriorityQueue.java
@@ -9,61 +9,71 @@ import java.util.Map;
 
 /**
  * Represents a generic priority queue.
+ *
  * @param <E> The type of elements in the priority queue.
  * @implNote The priority queue is implemented using a max-heap.
  */
 public final class PriorityQueue<E> implements AbstractQueue<E> {
-    private final MaxHeap<E> heap;
-    private final Map<E, Integer> indexMap;
+  private final MaxHeap<E> heap;
+  private final Map<E, Integer> indexMap;
 
-    /**
-     * Constructs a new {@link PriorityQueue} with the given comparator.
-     * @param comparator The comparator used to compare elements priority in the priority queue.
-     */
-    public PriorityQueue(Comparator<E> comparator) {
-        this.heap = new MaxHeap<>(comparator);
-        this.indexMap = new HashMap<>();
-    }
+  /**
+   * Constructs a new {@link PriorityQueue} with the given comparator.
+   *
+   * @param comparator The comparator used to compare elements priority in the priority queue.
+   */
+  public PriorityQueue(Comparator<E> comparator) {
+    this.heap = new MaxHeap<>(comparator);
+    this.indexMap = new HashMap<>();
+  }
 
-    /**
-     * Checks whether the priority queue is empty or not.
-     * @return {@code true} if the priority queue is empty, {@code false} otherwise.
-     * @implNote This operation has constant time complexity O(1).
-     */
-    @Override
-    public boolean empty() {
-        return heap.empty();
-    }
+  /**
+   * Checks whether the priority queue is empty or not.
+   *
+   * @return {@code true} if the priority queue is empty, {@code false} otherwise.
+   * @implNote This operation has constant time complexity O(1).
+   */
+  @Override
+  public boolean empty() {
+    return heap.empty();
+  }
 
-    @Override
-    public boolean push(@NotNull E element) {  // TODO
-        return false;
-    }
+  @Override
+  public boolean push(@NotNull E element) {  // TODO
+    return false;
+  }
 
-    /**
-     * Checks whether a specific element is present in the priority queue or not.
-     * The element shall not be {@code null}.
-     * @param element The element to check for existence.
-     * @return {@code true} if the element is present, {@code false} otherwise.
-     * @implNote This operation has constant time complexity O(1).
-     */
-    @Override
-    public boolean contains(@NotNull E element) {
-        return indexMap.containsKey(element);
-    }
+  /**
+   * Checks whether a specific element is present in the priority queue or not.
+   * The element shall not be {@code null}.
+   *
+   * @param element The element to check for existence.
+   * @return {@code true} if the element is present, {@code false} otherwise.
+   * @implNote This operation has constant time complexity O(1).
+   */
+  @Override
+  public boolean contains(@NotNull E element) {
+    return indexMap.containsKey(element);
+  }
 
-    @Override
-    public @Nullable E top() {  // TODO
-        return null;
-    }
+  /**
+   * Accesses the element at the top of the priority queue without removing it.
+   *
+   * @return The element at the top of the queue, or {@code null} if the queue is empty.
+   * @implNote This operation has constant time complexity O(1).
+   */
+  @Override
+  public @Nullable E top() {
+    return heap.root();
+  }
 
-    @Override
-    public void pop() {  // TODO
+  @Override
+  public void pop() {  // TODO
 
-    }
+  }
 
-    @Override
-    public boolean remove(@NotNull E element) {  // TODO
-        return false;
-    }
+  @Override
+  public boolean remove(@NotNull E element) {  // TODO
+    return false;
+  }
 }
