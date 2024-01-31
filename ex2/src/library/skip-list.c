e954943d6864170e7d3f168e9f1c9727899e1e97
Marco Orrù <marco.orru@edu.unito.it>
2024-01-31 17:00:38 +0100

    Ex2: new_skiplist

--- ex2/src/library/skip-list.c
+++ ex2/src/library/skip-list.c
@@ -1,1 +1,38 @@
+#include <assert.h>
 #include "skip-list.h"
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void new_skiplist(struct SkipList **list, size_t max_height, compare_fn compare) {
+    SkipList* skiplist;
+
+    assert(list);
+    assert(max_height >= 1);
+    assert(compare);
+
+    skiplist = (SkipList*)malloc(sizeof(SkipList));
+    skiplist->heads = NULL;
+    skiplist->max_level = 0;
+    skiplist->max_height = max_height;
+    skiplist->compare = compare;
+
+    *list = skiplist;
+}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void clear_skiplist(struct SkipList **list) {
+
+}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void insert_skiplist(struct SkipList *list, void *item) {
+
+}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+const void *search_skiplist(struct SkipList *list, void *item) {
+    return NULL;
+}
--- ex2/src/library/skip-list.h
+++ ex2/src/library/skip-list.h
@@ -1,1 +1,40 @@
 #pragma once
+#include <stdlib.h>
+
+/**
+ * @brief Function pointer type for element comparison in a SkipList.
+ *
+ * @remark A comparison function of this type should take two pointers to elements
+ * and return an integer value indicating their relative order:
+ * <ul>
+ *      <li>A negative value if the first element is less than the second.</li>
+ *      <li>Zero if the elements are equal</li>
+ *      <li>A positive value if the first element is greater than the second</li>
+ * </ul>
+ *
+ * @param a Pointer to the first element for comparison.
+ * @param b Pointer to the second element for comparison.
+ * @return An integer representing the comparison result.
+ */
+typedef int (*compare_fn)(const void *, const void *);
+
+typedef struct SkipList {
+    struct SkipListNode **heads;
+    size_t max_level;
+    size_t max_height;
+    compare_fn compare;
+} SkipList;
+
+typedef struct SkipListNode {
+    struct SkipListNode** next;
+    size_t size;
+    void* item;
+} SkipListNode;
+
+void new_skiplist(struct SkipList **list, size_t max_height, compare_fn compare);
+
+void clear_skiplist(struct SkipList **list);
+
+void insert_skiplist(struct SkipList *list, void *item);
+
+const void* search_skiplist(struct SkipList *list, void *item);
